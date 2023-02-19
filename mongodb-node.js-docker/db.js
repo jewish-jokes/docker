785b74727897a6700f183f9dae5d16e3cce6b10e
jewish-jokes <toshunets@gamil.com>
2023-02-19 20:12:04 +0300

    Error fixed

--- mongodb-node.js-docker/db.js
+++ mongodb-node.js-docker/db.js
@@ -1,11 +1,25 @@
 const mongoose = require('mongoose');
 
-const MONGO_USERNAME = 'root';
-const MONGO_PASSWORD = '123';
-const MONGO_HOSTNAME = '127.0.0.1';
-const MONGO_PORT = '27017';
-const MONGO_DB = 'penguins_info';
+const {
+  MONGO_USERNAME,
+  MONGO_PASSWORD,
+  MONGO_HOSTNAME,
+  MONGO_PORT,
+  MONGO_DB
+} = process.env;
+
+const options = {
+    useNewUrlParser: true,
+    connectTimeoutMS: 10000,
+};
+
+
 
 const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
 
-mongoose.connect(url, {useNewUrlParser: true});
+mongoose.connect(url, options).then( function() {
+  console.log('MongoDB is connected');
+})
+  .catch( function(err) {
+  console.log(err);
+});
